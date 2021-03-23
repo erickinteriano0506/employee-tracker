472d73d1012dd5c7e90db5685380e087566c803d
E Alexander <erick.interiano05@gmail.com>
2021-03-22 20:47:06 -0700

    Added inquirer prompts for options and select from to schema

--- server.js
+++ server.js
@@ -1,2 +1,31 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+    host: 'locahost',
+    port: 3350,
+    user: 'root',
+    password: 'password',
+    database: 'cms_db',
+});
+
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'options',
+            choices: [
+                "View employees",
+                "View Departments",
+                "View All Roles",
+                "View All Employees By Department",
+                "View All Employees By Role",
+                "Add Employee",
+                "Add Role",
+                "Update Employee Role",
+                "Add Department",
+                "Exit",
+            ]
+        }
+    ])
